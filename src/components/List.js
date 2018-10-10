f912742c92d56c279c0b42ecc7353bf559aba06d
Ihor Ruzhenko <deedeeqqs@gmail.com>
2018-10-10 14:38:46 +0300

    add router

---
+++ src/components/List.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class List extends Component {
+  render() {
+    return <h1>Hello</h1>;
+  }
+}
+
+export default withRouter(List);
