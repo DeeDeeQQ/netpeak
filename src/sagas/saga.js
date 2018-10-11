78c55c0efd72d067a3747eb676e6e8d0852dbdeb
Ihor Ruzhenko <deedeeqqs@gmail.com>
2018-10-11 18:36:51 +0300

    add post saga-logic

--- src/sagas/saga.js
+++ src/sagas/saga.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 export function* watcherSaga() {
   yield takeLatest("API_CALL_REQUEST_LIST", workerSagaList);
+  yield takeLatest("API_CALL_REQUEST_POST", workerSagaPost);
 }
 
 function getList() {
@@ -16,6 +17,49 @@ function* workerSagaList() {
   try {
     const response = yield call(getList);
     const data = response.data;
+    yield put({ type: "API_CALL_SUCCESS", data });
+  } catch (error) {
+    yield put({ type: "API_CALL_FAILURE", error });
+  }
+}
+
+function getPost(id) {
+  return axios({
+    method: "get",
+    url: `https://jsonplaceholder.typicode.com/posts/${id}`
+  });
+}
+
+function getUser(id) {
+  return axios({
+    method: "get",
+    url: `https://jsonplaceholder.typicode.com/users/${id}`
+  });
+}
+
+function getComments(id) {
+  return axios({
+    method: "get",
+    url: `https://jsonplaceholder.typicode.com/posts/${id}/comments`
+  });
+}
+
+function* workerSagaPost(action) {
+  try {
+    const { postResponse, commentsResponse } = yield all({
+      postResponse: call(getPost, action.payload),
+      commentsResponse: call(getComments, action.payload)
+    });
+    const post = postResponse.data;
+    const comments = commentsResponse.data;
+
+    const userId = post.userId;
+    const userResponse = yield call(getUser, userId);
+    const user = userResponse.data;
+
+    const data = { post, comments, user };
+    console.log(data);
+
     yield put({ type: "API_CALL_SUCCESS", data });
   } catch (error) {
     yield put({ type: "API_CALL_FAILURE", error });
