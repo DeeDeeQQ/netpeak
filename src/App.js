1b1e6bec215d43cc7d142b1b5d5c6e3d9dc26066
Ihor Ruzhenko <deedeeqqs@gmail.com>
2018-10-12 13:46:30 +0300

    add code splitting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Loadable from "react-loadable";
 
 import "./App.css";
 import List from "./components/List";
-import Post from "./components/Post";
+
+const LoadablePost = Loadable({
+  loader: () => import("./components/Post"),
+  loading: () => <div>Loading...</div>
+});
 
 const App = props => (
   <Router basename={props.path}>
     <div>
       <Route exact path="/" component={List} />
-      <Route path="/post/:postId" component={Post} />
+      <Route path="/post/:postId" component={LoadablePost} />
     </div>
   </Router>
 );
