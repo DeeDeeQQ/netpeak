39785f3844837af177b7cf448069c4fe0e4a1882
Ihor Ruzhenko <deedeeqqs@gmail.com>
2018-10-12 15:33:01 +0300

    add comment local storage

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,12 +3,48 @@ import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import styled from "react-emotion";
 import { Link } from "react-router-dom";
+import SimpleStorage from "react-simple-storage";
 
 class Post extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: []
+    };
+  }
+
+  updateInput(key, value) {
+    // update react state
+    this.setState({ [key]: value });
+  }
+
   componentDidMount() {
     const id = this.props.match.params.postId;
     this.props.post(id);
   }
+
+  addItem() {
+    // create a new item with unique id
+    const newItem = {
+      id: 1 + Math.random(),
+      postId: this.props.match.params.postId,
+      value: this.state.newItem.slice()
+    };
+
+    // copy current list of items
+    const list = [...this.state.list];
+
+    // add the new item to the list
+    list.push(newItem);
+
+    // update state with new list, reset the new item input
+    this.setState({
+      list,
+      newItem: ""
+    });
+  }
+
   render() {
     const { fetching, data, error } = this.props;
     const { comments, post, user } = { ...data };
@@ -38,6 +74,26 @@ class Post extends Component {
                   <p>{comment.body}</p>
                 </CommentsDiv>
               ))}
+              {this.state.list &&
+                this.state.list.map(
+                  comment =>
+                    comment.postId === this.props.match.params.postId && (
+                      <div>{comment.value}</div>
+                    )
+                )}
+              <div>{console.log(this.state)}</div>
+              <SimpleStorage parent={this} />
+              <input
+                type="text"
+                value={this.state.text}
+                onChange={e => this.updateInput("newItem", e.target.value)}
+              />
+              <button
+                onClick={() => this.addItem()}
+                disabled={!this.state.newItem.length}
+              >
+                &#43; Add
+              </button>
             </MainPostDiv>
           )
         )}
